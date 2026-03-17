1bcf1af49dc0b715c37bdd5761a132ea98ab97af
Alexsantossp71 <alex_santos_sp@yahoo.com.br>
2026-03-16 22:58:01 -0300

    Fix python executable in build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 # Instala as dependências
 echo "Instalando dependências..."
-python3.12 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Roda as migrations (Cria as tabelas no Neon Postgres)
 echo "Rodando migrations..."
-python3.12 manage.py migrate --noinput
+python3 manage.py migrate --noinput
 
 # Coleta arquivos estáticos
 echo "Coletando arquivos estáticos..."
-python3.12 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 
 echo "Build concluído com sucesso!"
